d7d15b4fcc44b8cf49a08b00944f26071ce2f34d
Mike Buonomo <mrb2590@gmail.com>
2019-01-27 17:04:21 -0500

    Bugfix

--- app/Http/Controllers/UserAggregateController.php
+++ app/Http/Controllers/UserAggregateController.php
@@ -66,7 +66,7 @@ class UserAggregateController extends Controller
 
             // Search of the date
             foreach ($data as $item) {
-                if (strtolower($item->created_at) == $day) {
+                if (strtolower($item->created_at) == strtolower($day)) {
                     $dates[$i]['created_at'] = $item->created_at;
                     $dates[$i]['total'] = $item->total;
                     break;
